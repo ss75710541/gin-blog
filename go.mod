9de454d59747650a4cd0da59a3038f0ec38362b5
ss75710541 <75710541@qq.com>
2019-06-04 18:15:48 +0800

    Gin实践 连载五 使用JWT进行身份校验

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.12
 require (
 	github.com/Unknwon/com v0.0.0-20190321035513-0fed4efef755
 	github.com/astaxie/beego v1.11.1
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.4.0
 	github.com/go-ini/ini v1.42.0
 	github.com/jinzhu/gorm v1.9.8
--- routers/router.go
+++ routers/router.go
@@ -1,7 +1,9 @@
 package routers
 
 import (
+	"gin-blog/middleware/jwt"
 	"gin-blog/pkg/setting"
+	"gin-blog/routers/api"
 	v1 "gin-blog/routers/api/v1"
 	"github.com/gin-gonic/gin"
 )
@@ -13,7 +15,10 @@ func InitRouter() *gin.Engine {
 
 	gin.SetMode(setting.RunMode)
 
+	r.GET("/auth", api.GetAuth)
+
 	apiv1 := r.Group("/api/v1")
+	apiv1.Use(jwt.JWT())
 	{
 		//获取标签列表
 		apiv1.GET("/tags", v1.GetTags)

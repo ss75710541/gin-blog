3222fda6df3cdb295f0b4c518c426b0fc037323f
ss75710541 <75710541@qq.com>
2019-06-10 18:43:26 +0800

    Gin实践-连载七-Golang优雅重启HTTP服务

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/Unknwon/com v0.0.0-20190321035513-0fed4efef755
 	github.com/astaxie/beego v1.11.1
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/fvbock/endless v0.0.0-20170109170031-447134032cb6
 	github.com/gin-gonic/gin v1.4.0
 	github.com/go-ini/ini v1.42.0
 	github.com/jinzhu/gorm v1.9.8
--- main.go
+++ main.go
@@ -4,19 +4,54 @@ import (
 	"fmt"
 	"gin-blog/pkg/setting"
 	"gin-blog/routers"
-	"net/http"
+	"github.com/fvbock/endless"
+	"log"
+	"syscall"
 )
 
 func main() {
-	router := routers.InitRouter()
+	endless.DefaultReadTimeOut = setting.ReadTimeout
+	endless.DefaultWriteTimeOut = setting.WriteTimeout
+	endless.DefaultMaxHeaderBytes = 1 << 20
+	endPoint := fmt.Sprintf(":%d", setting.HTTPPort)
 
-	s := &http.Server{
-		Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
-		Handler:        router,
-		ReadTimeout:    setting.ReadTimeout,
-		WriteTimeout:   setting.WriteTimeout,
-		MaxHeaderBytes: 1 << 20,
+	server := endless.NewServer(endPoint, routers.InitRouter())
+	server.BeforeBegin = func(add string) {
+		log.Printf("Actual pid is %d", syscall.Getpid())
 	}
 
-	s.ListenAndServe()
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Printf("Server err: %v", err)
+	}
+
+	//router := routers.InitRouter()
+	//
+	//s := &http.Server{
+	//	Addr:           fmt.Sprintf("%d", setting.HTTPPort),
+	//	Handler:        router,
+	//	ReadTimeout:    setting.ReadTimeout,
+	//	WriteTimeout:   setting.WriteTimeout,
+	//	MaxHeaderBytes: 1 << 20,
+	//}
+	//
+	//go func() {
+	//	if err := s.ListenAndServe(); err != nil {
+	//		log.Printf("Listen: %s\n", err)
+	//	}
+	//}()
+	//
+	//quit := make(chan os.Signal)
+	//signal.Notify(quit, os.Interrupt)
+	//<-quit
+	//
+	//log.Println("Shutdown Server ...")
+	//
+	//ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	//defer cancel()
+	//if err := s.Shutdown(ctx); err != nil {
+	//	log.Fatal("Server Shutdown:", err)
+	//}
+	//
+	//log.Println("Server exiting")
 }
